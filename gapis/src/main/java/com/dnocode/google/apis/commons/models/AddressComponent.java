2115b25e93fbd20c457852fa0d91a34bc8d27216
dnocode <ricceri.dino@gmail.com>
2015-04-24 18:01:09 +0200

    bug fix

--- gapis/src/main/java/com/dnocode/google/apis/commons/models/AddressComponent.java
+++ gapis/src/main/java/com/dnocode/google/apis/commons/models/AddressComponent.java
@@ -1,6 +1,8 @@
 
 package com.dnocode.google.apis.commons.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,71 +12,11 @@ import java.util.Map;
 
 public class AddressComponent implements Serializable{
 
-    private String longName;
-    private String shortName;
-    private List<String> types = new ArrayList<String>();
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    /**
-     * 
-     * @return
-     *     The longName
-     */
-    public String getLongName() {
-        return longName;
-    }
-
-    /**
-     * 
-     * @param longName
-     *     The long_name
-     */
-    public void setLongName(String longName) {
-        this.longName = longName;
-    }
-
-    /**
-     * 
-     * @return
-     *     The shortName
-     */
-    public String getShortName() {
-        return shortName;
-    }
-
-    /**
-     * 
-     * @param shortName
-     *     The short_name
-     */
-    public void setShortName(String shortName) {
-        this.shortName = shortName;
-    }
-
-    /**
-     * 
-     * @return
-     *     The types
-     */
-    public List<String> getTypes() {
-        return types;
-    }
-
-    /**
-     * 
-     * @param types
-     *     The types
-     */
-    public void setTypes(List<String> types) {
-        this.types = types;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
+    @SerializedName("long_name")
+    public String longName;
+    @SerializedName("short_name")
+    public String shortName;
+    public List<String> types = new ArrayList<String>();
+    public Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
 }
